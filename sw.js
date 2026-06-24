79b6d7be8392e308a0fbaa9a0e387e3603799bbf
herudia94-blip <herudia94@gmail.com>
2026-06-24 05:58:42 +0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,17 @@
+const CACHE_NAME = 'crisismap-v2';
+const ASSETS_TO_CACHE = [
+    '/',
+    '/index.html',
+    'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/leaflet.min.css',
+    'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.9.4/leaflet.min.js'
+];
+
 self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(function(cache) {
+            return cache.addAll(ASSETS_TO_CACHE);
+        })
+    );
     self.skipWaiting();
 });
 
@@ -6,7 +19,9 @@ self.addEventListener('activate', function(event) {
     event.waitUntil(
         caches.keys().then(function(cacheNames) {
             return Promise.all(
-                cacheNames.map(function(name) {
+                cacheNames.filter(function(name) {
+                    return name !== CACHE_NAME;
+                }).map(function(name) {
                     return caches.delete(name);
                 })
             );
@@ -16,5 +31,22 @@ self.addEventListener('activate', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-    event.respondWith(fetch(event.request));
-});
+    event.respondWith(
+        caches.match(event.request).then(function(cachedResponse) {
+            if (cachedResponse) {
+                return cachedResponse;
+            }
+            return fetch(event.request).then(function(networkResponse) {
+                if (event.request.url.includes('tile.openstreetmap.org')) {
+                    caches.open(CACHE_NAME).then(function(cache) {
+                        cache.put(event.request, networkResponse.clone());
+                    });
+                }
+                return networkResponse;
+            }).catch(function() {
+                if (event.request.mode === 'navigate') {
+                    return caches.match('/index.html');
+                }
+            });
+        })
+    );
